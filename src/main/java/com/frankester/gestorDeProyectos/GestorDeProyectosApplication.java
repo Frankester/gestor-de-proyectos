428c4480724fce562ad5cb24e63fea2ed2436f66
Franco Callero <fcallero@frba.utn.edu.ar>
2023-07-23 13:00:21 -0300

    feat: inicio del proyecto

---
+++ src/main/java/com/frankester/gestorDeProyectos/GestorDeProyectosApplication.java
@@ -0,0 +1,13 @@
+package com.frankester.gestorDeProyectos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorDeProyectosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorDeProyectosApplication.class, args);
+	}
+
+}
